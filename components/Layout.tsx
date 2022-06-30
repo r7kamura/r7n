812cafac4f4483445ae6d81f564d3c3a8b88849e
Ryo Nakamura <r7kamura@gmail.com>
2022-07-01 06:48:16 +0900

    Fix import type ReactNode

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { type ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
